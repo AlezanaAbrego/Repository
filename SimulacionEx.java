808dd5f8b96f688286015339b7f10e1882e38f2d
Alexia Abrego <alarabregode@ittepic.edu.mx>
2016-02-17 10:39:50 -0600

    Commit de verdad

---
+++ SimulacionEx.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class SimulacionEx{
+
+
+	public static void main (String args[]){
+	JFrame ventana = new JFrame("La simulación :O");
+	ventana.setSize(100,100);
+	ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	ventana.setVisible(true);
+	}
+
+
+
+
+
+
+
+
+}
